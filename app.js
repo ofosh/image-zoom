d4f89d8aca689715ae14db5a238933cebddb5e9d
ofosh <110522335+ofosh@users.noreply.github.com>
2023-09-18 17:04:17 +0100

    added an upload button

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const zoomIn = document.getElementById('zoomInBtn')
 const zoomOut = document.getElementById('zoomOutBtn')
 const image = document.querySelector("img");
+const imgCard = document.getElementById("img-card");
+
+const realFileBtn = document.getElementById("real-file");
+const customBtn = document.getElementById("custom-button")
+const customText = document.getElementById("custom-text")
 
 function zoomInBtn() {
   if(zoomIn && image) {
@@ -14,5 +19,12 @@ function zoomOutBtn() {
 
     image.style.width = `${image.width - 20}px`;
     image.style.height = `${image.height - 20}px`;
+  }
+}
+
+realFileBtn.onchange = function() {
+  imgCard.src = URL.createObjectURL(realFileBtn.files[0]);
+  if(realFileBtn.value) {
+    customText.innerHTML = realFileBtn.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)
   }
 }
